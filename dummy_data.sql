8eedcb9944b362da0d814dbaecab8db51b9ceffd
Fabbernat <h259147@stud.u-szeged.hu>
2023-11-11 12:46:01 +0100

    dummy_data.sql plusz 1 sor

--- dummy_data.sql
+++ dummy_data.sql
@@ -1,4 +1,28 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2023. Nov 01. 12:42
+-- Kiszolgáló verziója: 10.4.28-MariaDB
+-- PHP verzió: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS = @@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION = @@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `adatb_helyfoglalas`
+--
+CREATE DATABASE IF NOT EXISTS `adatb_helyfoglalas` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `adatb_helyfoglalas`;
+
 -- --------------------------------------------------------
 
 --
@@ -8,27 +32,19 @@ USE `adatb_helyfoglalas`;
 DROP TABLE IF EXISTS `felhasznalo`;
 CREATE TABLE `felhasznalo`
 (
-    `felhasznalonev` varchar(128) NOT NULL PRIMARY KEY,
-    `email`          varchar(128) NOT NULL UNIQUE,
-    `jelszo`         varchar(128),
-    `vezeteknev`     varchar(128) DEFAULT NULL,
-    `keresztnev`     varchar(128) DEFAULT NULL,
-    `szerep`         varchar(128) DEFAULT 'Nincs szerepe',
-    jegyek_darabszama    int          DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci COMMENT='Minden felhasználónak egyedi neve kell, hogy legyen ez regisztációkor ellenőrizzük';
+    `felhasznalonev`  varchar(128) NOT NULL PRIMARY KEY,
+    `email`           varchar(128) NOT NULL UNIQUE,
+    `jelszo`          varchar(128) NOT NULL,
+    `vezeteknev`      varchar(128) DEFAULT NULL,
+    `keresztnev`      varchar(128) DEFAULT NULL,
+    `szerep`          varchar(128) DEFAULT 'Nincs szerepe',
+    jegyek_darabszama int          DEFAULT 0
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_hungarian_ci COMMENT ='Minden felhasználónak egyedi neve kell, hogy legyen ez regisztációkor ellenőrizzük';
 
 -- --------------------------------------------------------
 
-
-
-
-DROP TABLE IF EXISTS `allomas`;
-CREATE TABLE `allomas`
-(
-    `allomasazonosito` INT AUTO_INCREMENT PRIMARY KEY,
-    `nev`              varchar(128) CHARACTER SET utf16 COLLATE utf16_hungarian_ci NOT NULL,
-    `varos`            varchar(32) CHARACTER SET utf16 COLLATE utf16_hungarian_ci  NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
 -- --------------------------------------------------------
 
@@ -39,13 +55,26 @@ CREATE TABLE `allomas`
 DROP TABLE IF EXISTS `jarat`;
 CREATE TABLE `jarat`
 (
-    `jaratazonosito` INT AUTO_INCREMENT,
+    `jaratazonosito` INT AUTO_INCREMENT PRIMARY KEY,
     `tipus`          varchar(64)  NOT NULL,
     `induloallomas`  varchar(128) NOT NULL,
     `celallomas`     varchar(128) NOT NULL,
     `datum`          DATE         NOT NULL DEFAULT current_timestamp(),
     `idopont`        TIMESTAMP    NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_hungarian_ci;
+
+
+DROP TABLE IF EXISTS `allomas`;
+CREATE TABLE `allomas`
+(
+    `allomasazonosito` INT AUTO_INCREMENT PRIMARY KEY,
+    `nev`              varchar(128) CHARACTER SET utf16 COLLATE utf16_hungarian_ci NOT NULL,
+    `varos`            varchar(32) CHARACTER SET utf16 COLLATE utf16_hungarian_ci  NOT NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_hungarian_ci;
 
 -- --------------------------------------------------------
 
@@ -56,10 +85,14 @@ CREATE TABLE `jarat`
 DROP TABLE IF EXISTS `jegy`;
 CREATE TABLE `jegy`
 (
-    `jegyazonosito` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `ar`            INT NOT NULL DEFAULT 0,
-    `elerhetodarab` INT NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+    `jaratazonosito` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `ar`             INT NOT NULL DEFAULT 0,
+    `elerhetodarab`  INT NOT NULL DEFAULT 0,
+    FOREIGN KEY (`jaratazonosito`) REFERENCES `jarat` (`jaratazonosito`)
+
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_hungarian_ci;
 
 -- --------------------------------------------------------
 
@@ -70,33 +103,42 @@ CREATE TABLE `jegy`
 DROP TABLE IF EXISTS `felhasznalo_jegyei`;
 CREATE TABLE `felhasznalo_jegyei`
 (
-    `jegyazonosito`  char(16) NOT NULL,
-    `jaratazonosito` char(16) NOT NULL,
-    `felhasznalonev` tinytext NOT NULL
-        REFERENCES jegy(jegyazonosito) REFERENCES jarat(jaratazonosito) REFERENCES felhasznalo(felhasznalonev)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+    `jegysorszam` INT AUTO_INCREMENT,
+    `jaratazonosito` INT          NOT NULL,
+    `felhasznalonev` VARCHAR(128) NOT NULL,
+    PRIMARY KEY (`jaratazonosito`, `felhasznalonev`),
+    FOREIGN KEY (`jaratazonosito`) REFERENCES `jarat` (`jaratazonosito`),
+    FOREIGN KEY (`felhasznalonev`) REFERENCES `felhasznalo` (`felhasznalonev`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_hungarian_ci;
 
--- -----------------------------------------
+/*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS = @OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION = @OLD_COLLATION_CONNECTION */;
 
-INSERT INTO allomas (nev, varos) VALUES
-('Budapest-Keleti pályaudvar', 'Budapest'),
-('Debrecen vasútállomás', 'Debrecen'),
-('Ferihegy repülőtér', 'Budapest'),
-('Népliget buszpályaudvar', 'Budapest');
-
-INSERT INTO felhasznalo (felhasznalonev, email, jelszo, vezeteknev, keresztnev, szerep) VALUES
-('user1', 'user1@example.com', 'password1', 'John', 'Doe', 'Felhasználó'),
-('admin1', 'admin1@example.com', 'adminpassword', 'Admin', 'Admin', 'Adminisztrátor');
+INSERT INTO felhasznalo (felhasznalonev, email, jelszo, vezeteknev, keresztnev, szerep, jegyek_darabszama) VALUES
+('user1', 'user1@example.com', 'password1', 'John', 'Doe', 'Felhasználó',0),
+('admin1', 'admin1@example.com', 'adminpassword', 'Admin', 'Admin', 'Adminisztrátor',0);
+('','','','','','',0),
 
 INSERT INTO jarat (tipus, induloallomas, celallomas, datum, idopont) VALUES
 ('Vonat', 'Budapest-Keleti pályaudvar', 'Debrecen vasútállomás', '2023-11-01', '08:00:00'),
 ('Repülő', 'Ferihegy repülőtér', 'Népliget buszpályaudvar', '2023-11-02', '10:00:00');
 
+
+INSERT INTO allomas (nev, varos) VALUES
+                                     ('Budapest-Keleti pályaudvar', 'Budapest'),
+                                     ('Debrecen vasútállomás', 'Debrecen'),
+                                     ('Ferihegy repülőtér', 'Budapest'),
+                                     ('Népliget buszpályaudvar', 'Budapest')
+;
+
 INSERT INTO jegy (ar, elerhetodarab) VALUES
 (1500, 20),
 (5000, 10);
 
-INSERT INTO felhasznalo_jegyei (jegyazonosito, jaratazonosito, felhasznalonev) VALUES
-('ABC123', 1, 'user1'),
-('XYZ456', 2, 'user1'),
-('123ABC', 1, 'admin1');
+INSERT INTO felhasznalo_jegyei (jegysorszam, jaratazonosito, felhasznalonev) VALUES
+(1, 1, 'user1'),
+(2, 2, 'user1'),
+(3, 1, 'admin1');
